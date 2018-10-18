e71b67487e339d1a485b8ef324f6589e749e2d39
Tao Zhong <taozhongimd@gmail.com>
2018-10-18 12:27:28 +0200

    Update deployApple.sh

--- deployAPPLE.sh
+++ deployAPPLE.sh
@@ -3,35 +3,11 @@
 RED='\033[0;31m'
 NC='\033[0m' # No Color
 
+#Version=0.8.4
 CMD=jpseditor
 
-if [ -d build ];then
-    echo "INFO: found build directory"
-    echo "INFO:${RED} rm build ${NC}"
-    rm -rf build
-    echo "INFO: mkdir build"
-    mkdir build
-fi
-
-echo "INFO: cd build"
-cd build
-
-echo "INFO: actual directory: ${PWD}"
-
-if [ -d ../bin ]; then
-    echo "INFO: found bin directory"
-    echo "INFO:${RED} remove ./bin directory ${NC}"
-    rm -rf ../bin
-fi
-
-echo "INFO: cmake .."
-cmake ..
-
-echo "INFO: make ${CMD}"
-make -j4
-
-echo "INFO: running  dynlibbundler .."
-dylibbundler -od -b -x ../bin/${CMD}.app/Contents/MacOS/${CMD} -d ../bin/${CMD}.app/Contents/libs/
+#echo "INFO: running  dynlibbundler .."
+#dylibbundler -od -b -x ../bin/${CMD}.app/Contents/MacOS/${CMD} -d ../bin/${CMD}.app/Contents/libs/
 
 echo "INFO: running macdeployqt .."
-macdeployqt ../bin/${CMD}.app -dmg
+macdeployqt ./qmake-build-release//${CMD}.app -dmg
