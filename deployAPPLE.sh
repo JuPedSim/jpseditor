3d522ea20cee21c5a2bf0d22769e6822a3f5d7e3
Tao Zhong <taozhongimd@gmail.com>
2018-10-24 17:26:51 +0200

    Update deployApple.sh

--- deployAPPLE.sh
+++ deployAPPLE.sh
@@ -4,10 +4,12 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 
 #Version=0.8.4
-CMD=jpseditor
+#Change Build folder before running script
+
+release=Repos/jpseditor/qmake-build-release
 
 #echo "INFO: running  dynlibbundler .."
 #dylibbundler -od -b -x ../bin/${CMD}.app/Contents/MacOS/${CMD} -d ../bin/${CMD}.app/Contents/libs/
 
 echo "INFO: running macdeployqt .."
-macdeployqt ./qmake-build-release//${CMD}.app -dmg
+macdeployqt ${release}/JPSeditor.app -dmg
