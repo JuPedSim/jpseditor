4b4e79005e5ca81669a11c85732a5f3f54c89404
Tao Zhong <taozhongimd@gmail.com>
2019-01-25 11:36:26 +0000

    Update jpseditor.rb

--- jpseditor.rb
+++ jpseditor.rb
@@ -1,7 +1,8 @@
 class Jpseditor < Formula
   desc "A graphical user interface to create the geometry of a scenario simulated by JuPedSim"
-  homepage "https://www.jupedsim.org/"
-  head "https://github.com/JuPedSim/jpseditor.git" :branch => "version087"
+  homepage "https://www.jupedsim.org/jpseditor/"
+#   head "https://github.com/JuPedSim/jpseditor.git" :branch => "version087"
+  head "https://gitlab.version.fz-juelich.de/jupedsim/jpseditor.git" :branch => "version087"
 
   depends_on "cmake" => :build
   depends_on "qt"
